9d54221954f1039d83a6b64c9870391b87cc31e0
januryga <lolzatu2@gmail.com>
2015-09-21 02:42:07 +0200

    refactored, created a function to get text content of element with given css selector

--- tracker.py
+++ tracker.py
@@ -7,8 +7,12 @@ from sys import exit
 
 page = requests.get('http://www.example.com')
 tree = html.fromstring(page.text)
-expression = HTMLTranslator().css_to_xpath('div')
 
-#title = tree.xpath(expression + '/text()')
-title = [e.text_content() for e in tree.xpath(expression)]
+def get_content_of(css_selector):
+# Returns text contents of html elements specified with a css selector
+    expression = HTMLTranslator().css_to_xpath(css_selector)
+    extract = [e.text_content() for e in tree.xpath(expression)]
+    return extract
+
+title = get_content_of('div > h1')
 print(title)
