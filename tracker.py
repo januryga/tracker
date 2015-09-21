b8bff51fa66c9fc54eb78032ba64f3eae3597b46
januryga <lolzatu2@gmail.com>
2015-09-21 03:31:45 +0200

    switched model: get_news items fetches all news items from given url and css path

--- tracker.py
+++ tracker.py
@@ -5,14 +5,16 @@ from cssselect import HTMLTranslator
 from lxml import html
 from sys import exit
 
-page = requests.get('http://www.example.com')
-tree = html.fromstring(page.text)
 
-def get_content_of(css_selector):
-# Returns text contents of html elements specified with a css selector
+def get_news_items(url, css_selector):
+# Returns array of news item Elements with given css_selector from given url.
+    page = requests.get(url)
+    tree = html.fromstring(page.text)
+
     expression = HTMLTranslator().css_to_xpath(css_selector)
     extract = [e.text_content() for e in tree.xpath(expression)]
     return extract
 
-title = get_content_of('div > h1')
-print(title)
+news = get_news_items('http://chelmiec.pl/aktualnosci.html', 'div.news')
+printable_news = [item.encode('ascii', 'ignore') for item in news]
+print(printable_news)
